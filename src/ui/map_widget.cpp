c80f43bbd848369d076fbf84e87fbc7346523ebf
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-12-27 20:00:49 +0200

    fixing MapWidget to render everything, WIP

--- src/ui/map_widget.cpp
+++ src/ui/map_widget.cpp
@@ -7,19 +7,63 @@ MapWidget::MapWidget(int i_size, QWidget* parent): QWidget(parent) {
 }
 
 
-  //mapScene->setMap(scenarioService.get_map());
-  //std::vector<std::string> get_map();
 void MapWidget::setMap(std::vector<std::string>  citymap) {
-  
+  size = citymap.size();
+  storage.assign(size * size, 0);
+  img = QImage(size, size, QImage::Format_RGB32);
+  for (int y=0; y < size; y++) {
+    for (int x=0; x < size; x++) {
+      if (citymap[y][x]=='@') {
+        storage[xy2int(x, y, size)] |= WALL;
+      }
+    } 
+  }
+  renderMap();
 }
 
-void MapWidget::setScenario(Scenario scenario) {
 
+void MapWidget::setScenario(Scenario scenario) {
 }
 
 void MapWidget::paintEvent(QPaintEvent*) {
     QPainter p(this);
+    p.scale(mapScale, mapScale);
     p.drawImage(0, 0, img);
+}
+
+void MapWidget::renderMap() {
+  int i = 0;
+  for (int y=0;y < img.height();y++) {
+    QRgb* row = reinterpret_cast<QRgb*>(img.scanLine(y));
+    for (int x=0; x < img.width(); x++) {
+      row[x] = colorPixel(storage[i]);
+      i++;
+    } 
+  }
+  resize(img.height()*mapScale, img.width()*mapScale);
+  update();
+}
+
+QRgb MapWidget::colorPixel(uint8_t byte) {
+  if (showAstar) {
+
+  }
+  if (showFringe) {
+
+  }
+  if (scenario.has_value()) {
+    if (byte & START) {
+      return DEFAULT_BIT_PALETTE[2];
+    }
+    if (byte & GOAL) {
+      return DEFAULT_BIT_PALETTE[3];
+    }
+  }
+  if (byte & WALL) {
+    // std::cout << "wall" << std::endl;
+    return DEFAULT_BIT_PALETTE[1];
+  }
+  return DEFAULT_BIT_PALETTE[0];
 }
 
 int MapWidget::showHideAstar() {
@@ -73,4 +117,15 @@ void MapWidget::clear_astar() {
       storage[xy2int(x, y, size)] &= CLEAR_ASTAR;
     }
   }
+}
+
+void MapWidget::drawCross(const int& x, const int& y, const uint& index_or_rgb) {
+  if (size < 2) {
+    return;
+  }
+  QPainter painter(this);
+  
+  painter.setPen(QColor(index_or_rgb));
+  painter.drawLine(std::max(0, x-2), y, std::min(size - 1, x + 2), y);
+  painter.drawLine(x, std::max(0, y-2), x, std::min(size - 1, y+2));
 }
--- src/ui/map_widget.h
+++ src/ui/map_widget.h
@@ -4,6 +4,8 @@
 #include "conversions.h"
 #include <QWidget>
 #include <QPainter>
+#include <cstdint>
+#include <optional>
 /*
 bit scheme
 bit 7: open/closed renamed to WALL since that's what we might draw
@@ -33,6 +35,18 @@ constexpr std::uint8_t CLEAR_ASTAR = ~(VISIT_A | EXPAND_A);
 * index = size * y + x
 */
 
+static constexpr std::array<QRgb, 9> DEFAULT_BIT_PALETTE = {
+    qRgb(255, 255, 255),
+    qRgb(0,   0,   0),     // bit 0: black
+    qRgb(255, 105, 180),   // bit 1: pink
+    qRgb(0,   255, 255),   // bit 2: cyan
+    qRgb(255, 255, 0),     // bit 3: yellow
+    qRgb(0,   255, 0),     // bit 4: green
+    qRgb(255, 165, 0),     // bit 5: orange
+    qRgb(0,   120, 255),   // bit 6: blue
+    qRgb(200, 200, 200)    // bit 7: light gray
+};
+
 class MapWidget : public QWidget {
     Q_OBJECT;
 public:
@@ -50,7 +64,14 @@ private:
     QImage img;
     std::vector<uint8_t> storage;
     int size;
+    bool showFringe;
+    bool showAstar;
+    std::optional<Scenario> scenario;
+    int mapScale = 5;
 
+    QRgb colorPixel(uint8_t byte);
+    void renderMap();
+    void drawCross(const int& x, const int& y, const uint& index_or_rgb);
     // void setMap(std::vector<std::string>  citymap);
     void visit_astar(QVector<QPoint> vec);
     void visit_fringe(QVector<QPoint> vec);
