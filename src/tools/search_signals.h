76f128ac60bafee9b1f6bc1063b6ebe7ae7f701b
Samuli Nikkilä <sanikk@users.noreply.github.com>
2026-01-07 04:05:57 +0200

    SearchSignals class

--- src/tools/search_signals.h
+++ src/tools/search_signals.h
@@ -1,11 +1,18 @@
 #pragma once
+
 #include "retval.h"
+
 #include <QObject>
 
 
 class SearchSignals : public QObject {
 
-    Q_OBJECT;
+    Q_OBJECT
+
+public:
+    inline void node_visited(int x, int y) { emit visit(x, y); }
+    inline void node_expanded(int x, int y) { emit expand(x, y); }
+    inline void search_finished(RetVal ret) { emit finished(ret); }
 
 signals:
     void visit(int x, int y);
